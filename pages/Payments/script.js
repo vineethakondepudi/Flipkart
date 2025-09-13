f9f1393f7092d202cbcf75d39468e39f23011f92
root <root@ip-172-31-45-92.ap-south-1.compute.internal>
2025-09-13 00:50:37 +0000

    Remove all files

--- pages/Payments/script.js
+++
@@ -1,5 +0,0 @@
-// to apply styling to the first paragraph that is immediately followed by a ul element
-
-document.querySelectorAll('p + ul').forEach(ul => {
-    ul.previousElementSibling.classList.add('highlight');
-});
